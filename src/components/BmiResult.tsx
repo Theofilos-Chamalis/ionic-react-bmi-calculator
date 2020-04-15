1ca76bdf70dc2b51af974df7b384248f1c141214
Theofilos Chamalis <t.chamalis@vieconnect.io>
2020-04-15 18:20:39 +0300

    Override the result card style of the BmiResult component using css

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { IonCard, IonCardContent, IonRow, IonCol } from '@ionic/react';
 
+import './BmiResult.css'
+
 const BmiResult: React.FC<{ bmiResult: number }> = props => {
     return (
         <IonRow>
             <IonCol>
-                <IonCard>
+                <IonCard id="result-card">
                     <IonCardContent className="ion-text-center">
                         <h2>Your Body Mass Index</h2>
                         <h3>{props.bmiResult.toFixed(2)}</h3>
